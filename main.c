8c2d1656b3fc22bcc24ee97bd7ca9a96ae3294f1
alphonsus0 <alonso.misael.acuna.pezo7018@gmail.com>
2025-08-05 22:15:51 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <math.h>
+#include <locale.h>
 int main()
 {
-    printf("Hello world!\n");
-    int i = 1;
-    int batata = 10;
-    while (batata>0)
-        {
-        printf("Temos %i batatas\n",batata);
-        if (batata>0)
-            {
-            batata=batata-i;
-            printf("Eu quero uma batata!\n");
-            }
-        }
-
-    printf("Acabaram as batatas!\n");
-    printf("Volto outra hora...");
+    setlocale(LC_ALL, "Portuguese");
+    printf("MINI CALCULADORA DE INTEIROS: N1 e N2\n");
+    //entrada de dados
+    int N1, N2;
+    printf("VALOR DE N1:");
+    scanf("%i",&N1);
+    printf("VALOR DE N2:");
+    scanf("%i",&N2);
+    printf("ADIÇÃO: N1 + N2 = %i + %i = %i\n", N1, N2, N1 + N2);
+    printf("SUBTRAÇÃO: N1 - N2 = %i - %i = %i \n", N1, N2, N1 - N2);
+    printf("MULTIPLICAÇÃO: N1 * N2 = %i * %i = %i \n", N1, N2, N1 * N2);
+    printf("DIVISÃO: N1 * N2 = %i / %i = %.1f \n", N1, N2, 1.0*N1 / N2);
+    printf("DIVISÃO: N1 * N2 = %i / %i = %.1f \n", N1, N2,(float) N1 / N2);
+    printf("QUOCIENTE (int): N1 div N2 = %i / %i = %i \n", N1, N2, N1 / N2);
+    printf("RESTO (int): N1 mod N2 = %i %% %i = %i \n", N1, N2, N1 % N2);
+    printf("POTENCIAÇÃO: N1 elevado N2 = %i elevado %i = %.1f \n", N1, N2, pow(N1, N2));
+    printf("RADICIAÇÃO: N1 raiz N2 = %i raiz %.1f = %.1f \n", N1, 1.0/N2, pow(N1, 1.0 /N2));
 
     return 0;
 }
